d55456bfe1df6876717916a1d748baf9b528f941
chunghyunleeme <chunghyun.dev@gmail.com>
2024-06-13 01:17:37 +0900

    SolutionConfig 에서 의존성 주입

--- src/main/SolutionConfig.java
+++ src/main/SolutionConfig.java
@@ -5,11 +5,8 @@ import main.자료구조.P11660_구간합구하기2;
 import main.자료구조.P11720_숫자의합;
 import main.자료구조.P1546_평균;
 
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Solution solution = SolutionConfig.getSolution();
-        solution.solve();
-    }
+public class SolutionConfig {
+    public static Solution getSolution() {
+        return new P11660_구간합구하기2();
+    };
 }
