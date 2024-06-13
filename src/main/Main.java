0be28ae6a42d4ff9bf3385dd14d56ed1e1ab6748
chunghyunleeme <chunghyun.dev@gmail.com>
2024-06-13 10:06:43 +0900

    SolutionConfig 함수명 수정

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,10 @@
 package main;
 
-import main.자료구조.P11659_구간합구하기;
-import main.자료구조.P11660_구간합구하기2;
-import main.자료구조.P11720_숫자의합;
-import main.자료구조.P1546_평균;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Solution solution = SolutionConfig.getSolution();
+        Solution solution = SolutionConfig.solution();
         solution.solve();
     }
 }
--- src/main/SolutionConfig.java
+++ src/main/SolutionConfig.java
@@ -6,7 +6,7 @@ import main.자료구조.P11720_숫자의합;
 import main.자료구조.P1546_평균;
 
 public class SolutionConfig {
-    public static Solution getSolution() {
+    public static Solution solution() {
         return new P11660_구간합구하기2();
     };
 }
